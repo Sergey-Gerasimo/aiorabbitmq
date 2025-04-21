e284f77a1e4b95d57a71a9abf977f244303eb9a3
Sergey-Gerasimo <gerasimovsergey2004@gmail.com>
2025-04-22 00:35:22 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="aiorabbitmq",
+    version="0.1.0",
+    author="Sergey",
+    author_email="gerasimovsergey2004@gmail.com",
+    description="Short description of your package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Sergey-Gerasimo/aiorabbitmq.git",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    python_requires=">=3.7",
+    install_requires=[
+        "aio-pika>=9.5.5",
+        "aiormq>=6.8.1",
+        "pydantic>=1.9.0",
+        "typing-extensions>=4.3.0",
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=6.0",
+            "mypy>=0.910",
+            "flake8>=4.0",
+            "black>=22.0",
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries",
+        "Framework :: AsyncIO",
+    ],
+)
