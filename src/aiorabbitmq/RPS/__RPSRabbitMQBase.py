a175a6279cc4f5bb4e86850ea4d861ee1c5940e5
Sergey-Gerasimo <gerasimovsergey2004@gmail.com>
2025-04-24 12:37:53 +0300

    add setuplogger

--- src/aiorabbitmq/RPS/__RPSRabbitMQBase.py
+++ src/aiorabbitmq/RPS/__RPSRabbitMQBase.py
@@ -13,6 +13,11 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+def set_up_logger(new_logger): 
+    global logger
+    logger = new_logger
+    
+
 class NoCorrelationIDExeptin(Exception): pass 
 
 class NoCorrelationIDException(Exception): pass
